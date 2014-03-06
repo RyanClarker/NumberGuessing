a88f9d11e971a71e02201bc53b94bd7ab5815392
RyanClarker <clarke.ryan24@gmail.com>
2014-03-06 10:09:41 -0500

    New

---
+++ NumberGame/NumberGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int guess, low = 0, high = 100, c;
+            while (true)
+            {
+                guess =  (high + low) / 2;
+                Console.WriteLine("Is {0} your number? (h, l, c)", guess);
+                input = Console.ReadLine();
+                if (input == "l")
+                {
+                    high = guess - 1;
+                }
+                if (input == "h")
+                {
+                    low = guess + 1;
+                }
+                if (input == "c")
+                {
+                    break;
+                }
+                
+            }
+        }
+    }
+}
+
